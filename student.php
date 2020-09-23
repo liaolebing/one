12fec2ce3480e503d0244f934abf76dc78179342
Yingxianliao <1915726270@qq.com>
2020-09-23 23:01:26 +0800

    Add a Constructor

---
+++ student.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of student
+ *
+ * @author 18481
+ */
+class student {
+    function construct() {
+$this->surname = '';
+$this->first_name = '';
+$this->emails = array();
+$this->grades = array();
+$this->status = '';
+}
+function add_email($which,$address) {
+$this->emails[$which] = $address;
+}
+function add_grade($grade) {
+$this->grades[] = $grade;
+}
+function average() {
+$total = 0;
+foreach ($this->grades as $value)
+$total += $value;
+return $total / count($this->grades);
+}
+function toString() {
+$result = $this->first_name . ' ' . $this->surname;
+$result .= ' ('.$this->average().")\n";
+foreach($this->emails as $which=>$what)
+$result .= $which . ': '. $what. "\n";
+$result .= "\n";
+return '<pre>'.$result.'</pre>';
+}
+    //put your code here
+}
